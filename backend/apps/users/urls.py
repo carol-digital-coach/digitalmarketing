84f32c516a235e5b832a4518722eb8fa8fbefc7f
Rensytimsy <rensytimsy03@gmail.com>
2025-11-28 21:17:22 +0300

    auth flow

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -5,10 +5,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
+from . import views
 
 urlpatterns = [
     path("signup/", views.create_user_account, name="create-account"),
-    path("signin/", TokenObtainPairView.as_view(), name="signin"),
+    path("signin/", views.CustomTokenObatainPairView.as_view(), name="signin"),
     path("verify/token/", TokenVerifyView.as_view(), name="verify-token"),
-    path("refresh/token/", TokenRefreshView.as_view(), name="refresh-token")
+    path("refresh/token/", TokenRefreshView.as_view(), name="refresh-token"),
+    path("get-users/", views.get_user, name="get-user"),
+    path("get-cookie/", views.CustomTokenObatainPairView.as_view(), name="get-cookie"),
+    path("get-user/", views.get_authenticate_user, name="user-auth")
 ]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -10,6 +10,9 @@ from . import (
     serialilzers,
     managers
 )
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 
 import json
 import requests
@@ -36,3 +39,84 @@ def create_user_account(request):
         return response.Response({
             "error": str(e)
         }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        
+@decorators.api_view(["GET"])
+def get_user(request):
+    try:
+        users = models.PbUser.objects.all()
+        users_data = serialilzers.UserModelSerializer(users, many=True)
+        return response.Response({
+            "users": users_data.data
+        }, status=status.HTTP_200_OK)
+    except Exception as e:
+        return response.Response({
+            "message": "Users not found",
+            "error": str(e)
+        }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        
+class CustomTokenObatainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        if response.status_code == status.HTTP_200_OK:
+            access_token = response.data["access"] #type: ignore
+            refresh_token = response.data["refresh"] #type: ignore
+            
+            response.set_cookie(
+                key="access_cookie",
+                value=access_token,
+                httponly=True,
+                samesite="Lax",
+                max_age=1800,
+                secure=True
+            )
+            
+            response.set_cookie(
+                key="refresh_cookie",
+                value=refresh_token,
+                httponly=True,
+                samesite="Lax",
+                max_age=30480,
+                secure=True
+            )
+            
+            
+            
+        # del response.data["access"]  #type: ignore
+        # del response.data["refresh"]  #type: ignore  
+        return response
+    
+
+class CookieAuthentication(JWTAuthentication):
+    def authenticate(self, request):
+        access_token = request.COOKIES.get("access_cookie")
+        
+        if access_token is None:
+            return None
+        
+        validate_token = self.get_validated_token(access_token)
+        return self.get_user(validate_token), validate_token
+
+
+    
+@decorators.api_view(["GET"])
+@decorators.authentication_classes([CookieAuthentication])
+@decorators.permission_classes([permissions.IsAuthenticated])
+def get_authenticate_user(request):
+    user = request.user
+    if user:
+        return response.Response({
+            "message": "authenticated",
+            "user" : {
+                "username" : user.username,
+                "email": user.email,
+                "super_user": user.is_superuser,
+            }
+        }, status=status.HTTP_200_OK)
+        
+    return response.Response({
+        "message" : "No user found, user unauthenticated"
+    }, status=status.HTTP_400_BAD_REQUEST)
+
+
