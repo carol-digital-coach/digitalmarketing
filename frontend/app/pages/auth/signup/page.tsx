4897b3464a664c83d2ce27780ceabd87b582432a
Rensytimsy <rensytimsy03@gmail.com>
2025-12-11 00:39:18 +0300

    updated services page

--- frontend/app/pages/auth/signup/page.tsx
+++ frontend/app/pages/auth/signup/page.tsx
@@ -37,7 +37,7 @@ export default function SignUpPage() {
 
     const OnSubmit = async(values: z.infer<typeof UserSignUpSchema>) => {
         try{
-            const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL_LIVE}`, values)
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL_LIVE}users/signup/`, values)
             toast.success("Account created!")
             setTimeout(() => {
                 redirect("/pages/auth/signin")
